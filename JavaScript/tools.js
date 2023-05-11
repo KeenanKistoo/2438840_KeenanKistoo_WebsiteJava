24968f1e6ff0c313a76c92de3831fdeafa975c04
KeenanKistoo <kistook19@gmail.com>
2023-05-11 14:28:07 +0200

    Margin issues
    The heading is surrounded by white space.
    I need to find a fix for it. Last time I had to start from scratch but hopefully that's not the case this time.

--- JavaScript/tools.js
+++ JavaScript/tools.js
@@ -1,12 +1,1 @@
-const title = document.querySelector(".title");
-const heading = document.getElementById("text-head");
-const logo = document.getElementById("logo-head");
 
-title.addEventListener("mouseenter", () => {
-  heading.setAttribute("class", "show-logo");
-  console.log("Working");
-});
-
-//title.addEventListener("mouseleave", () => {
-title.innerHTML = "K";
-//});
