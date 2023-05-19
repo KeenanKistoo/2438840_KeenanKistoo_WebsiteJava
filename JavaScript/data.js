d960c5a6a3954701a48c53a63dc8cc4b2d4d84c7
KeenanKistoo <kistook19@gmail.com>
2023-05-19 22:46:12 +0200

    Nav bar js code refined

---
+++ JavaScript/data.js
@@ -0,0 +1,31 @@
+const details = [
+  {
+    title: "Blog Section",
+    classActive: "home-links active",
+    classInactive: "home-links",
+  },
+  {
+    title: "Design Section",
+    classActive: "home-links active",
+    classInactive: "home-links",
+  },
+];
+
+const code = `<section class="heading">
+<h1 class="main-head">${details[0].title}</h1>
+</section>
+<section class="tabs">
+<!--Container-->
+<ul class="links">
+  <!--Indvidual Links-->
+  <li>
+    <a class="${details[0].classInactive}" href="/index.html">Homepage</a>
+  </li>
+  <li><a class="${details[0].classActive}" href="/HTML/blogs.html">Blogs</a></li>
+  <li><a class="home-links" href="/HTML/design.html">Designs</a></li>
+  <li>
+    <a class="home-links" href="/HTML/portfolio.html">Portfolio</a>
+  </li>
+  <li><a class="home-links" href="/HTML/essays.html">Essays</a></li>
+</ul>
+</section>`;
