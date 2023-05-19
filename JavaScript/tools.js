178f02be9b52ec17f98820f5510c3de0bbe95eab
KeenanKistoo <kistook19@gmail.com>
2023-05-19 10:27:41 +0200

    Nav bar works
    I have completed the nav bar for the design section, I just need to manage the different parts and reduce the amount of code

--- JavaScript/tools.js
+++ JavaScript/tools.js
@@ -1,1 +1,22 @@
-let navBar = document.querySelector("nav-bar");
+let navBar = document.querySelector(".nav-bar");
+
+function SetNavBar() {
+  navBar.innerHTML = `<section class="heading">
+  <h1 class="main-head">${"Design Section"}</h1>
+</section>
+<section class="tabs">
+  <!--Container-->
+  <ul class="links">
+    <!--Indvidual Links-->
+    <li>
+      <a class="home-links active" href="/index.html">Homepage</a>
+    </li>
+    <li><a class="home-links" href="/HTML/blogs.html">Blogs</a></li>
+    <li><a class="home-links" href="/HTML/design.html">Designs</a></li>
+    <li>
+      <a class="home-links" href="/HTML/portfolio.html">Portfolio</a>
+    </li>
+    <li><a class="home-links" href="/HTML/essays.html">Essays</a></li>
+  </ul>
+</section>`;
+}
